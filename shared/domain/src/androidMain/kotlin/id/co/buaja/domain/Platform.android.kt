9dc156dd93d9df360c632273b03eee7d95903a6a
Egi10 <egifcb@gmail.com>
2023-08-09 08:29:15 +0700

    feat(modular) delete default platform in module

--- shared/domain/src/androidMain/kotlin/id/co/buaja/domain/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package id.co.buaja.domain
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
